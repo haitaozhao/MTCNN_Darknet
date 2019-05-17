2aaae7e17ad689aa62e4e17e1e0a319614bc2789
louishsu <is.louishsu@foxmail.com>
2019-05-17 21:06:05 +0800

    make install

---
+++ make_install.sh
@@ -0,0 +1,29 @@
+# compile darknet
+cd darknet
+make
+echo "Darknet compiled! "
+echo "============================================="
+
+# generate mtcnn weights
+cd mtcnn && mkdir weights
+cd ..
+cd torch_mtcnn
+python extract_weights.py
+cd ..
+echo "Weights generated! "
+echo "============================================="
+
+# compile mtcnn
+cd mtcnn
+mkdir build
+cd build
+cmake .. && make
+cd ..
+echo "============================================="
+
+echo "MTCNN compiled!  You can use MTCNN like: "
+echo "	./mtcnn detect <image path>"
+
+
+
+
